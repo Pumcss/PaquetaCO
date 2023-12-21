8b2841665ab3f6a3c1c3b763563234c9772e61bf
anthony <a.fromage@lyon.ort.asso.fr>
2023-12-21 09:54:29 +0100

    modif BO DOA +page mes info

--- src/Model/DAO/TuteurEcoleDAO.php
+++ src/Model/DAO/TuteurEcoleDAO.php
@@ -2,6 +2,11 @@
 
 namespace DAO;
 
+use BO\TuteurEcoleBO;
+use PDO;
+use PDOException;
+
+require_once 'Database.php';
 require_once 'TuteurEcoleBO.php';
 
 class TuteurEcoleDAO {
@@ -14,7 +19,8 @@ class TuteurEcoleDAO {
 
     public function create(TuteurEcoleBO $tuteur) {
         try {
-            $query = "INSERT INTO tuteurecole (idTut, nomTut, prenomTut, numTut, mailTut, nbalterTut, loginTut, mdpTut, roleTut) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            $query = "INSERT INTO tuteur_ecole (idTut, nomTut, prenomTut, numTut, mailTut, nbalterTut, loginTut, mdpTut, roleTut) 
+                      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
             $stmt = $this->conn->prepare($query);
             $stmt->execute([$tuteur->getIdTut(), $tuteur->getNomTut(), $tuteur->getPrenomTut(), $tuteur->getNumTut(), $tuteur->getMailTut(), $tuteur->getNbalterTut(), $tuteur->getLoginTut(), $tuteur->getMdpTut(), $tuteur->getRoleTut()]);
             return true;
@@ -26,7 +32,7 @@ class TuteurEcoleDAO {
 
     public function read($idTut) {
         try {
-            $query = "SELECT * FROM tuteurecole WHERE idTut = ?";
+            $query = "SELECT * FROM tuteur_ecole WHERE idTut = ?";
             $stmt = $this->conn->prepare($query);
             $stmt->execute([$idTut]);
             $result = $stmt->fetch(PDO::FETCH_ASSOC);
@@ -44,7 +50,7 @@ class TuteurEcoleDAO {
 
     public function update(TuteurEcoleBO $tuteur) {
         try {
-            $query = "UPDATE tuteurecole SET nomTut = ?, prenomTut = ?, numTut = ?, mailTut = ?, nbalterTut = ?, loginTut = ?, mdpTut = ?, roleTut = ? WHERE idTut = ?";
+            $query = "UPDATE tuteur_ecole SET nomTut = ?, prenomTut = ?, numTut = ?, mailTut = ?, nbalterTut = ?, loginTut = ?, mdpTut = ?, roleTut = ? WHERE idTut = ?";
             $stmt = $this->conn->prepare($query);
             $stmt->execute([$tuteur->getNomTut(), $tuteur->getPrenomTut(), $tuteur->getNumTut(), $tuteur->getMailTut(), $tuteur->getNbalterTut(), $tuteur->getLoginTut(), $tuteur->getMdpTut(), $tuteur->getRoleTut(), $tuteur->getIdTut()]);
             return true;
@@ -56,7 +62,7 @@ class TuteurEcoleDAO {
 
     public function delete($idTut) {
         try {
-            $query = "DELETE FROM tuteurecole WHERE idTut = ?";
+            $query = "DELETE FROM tuteur_ecole WHERE idTut = ?";
             $stmt = $this->conn->prepare($query);
             $stmt->execute([$idTut]);
             return true;
